d1d67b00c297b7e19e53750e0fd18eb59b5f0780
jamesrbluong <128750662+jamesrbluong@users.noreply.github.com>
2023-04-28 01:11:06 -0400

    Started merge project
    Began combining attributes from different branches

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System.Linq;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        private Cart cart;
+
+        public CartController(IProductRepository repo, Cart cartService)
+        {
+            repository = repo;
+            cart = cartService;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToActionResult AddToCart(int productId, int quantity, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.AddItem(product, quantity);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCart(int productId, int quantity, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.UpdateItem(product, quantity);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+    }
+}
